8eb27a066f2cba8838d7d382cdf81dd5d77e99d1
Sean Morgan <insideseanshead@gmail.com>
2022-01-10 13:06:07 -0800

    added bootstrap and added rows and col

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import CharacterDescription from "./components/CharacterDescription";
+import CharacterStats from "./components/CharacterStats";
 import Header from "./components/Header";
 import { CharacterProvider } from "./context/CharacterContext";
+import { Row, Col } from "react-bootstrap";
 
 function App() {
   return (
     <CharacterProvider>
       <div className="container">
-        <Header />
-        <CharacterDescription />
+        <Row>
+          <Col>Special Abilities and Cyphers</Col>
+          <Col>
+          <Header />
+          <CharacterDescription />
+          <CharacterStats />
+          </Col>
+          <Col>Skills/Inabilities, Equipment, Attacks</Col>
+        </Row>
       </div>
     </CharacterProvider>
   );
