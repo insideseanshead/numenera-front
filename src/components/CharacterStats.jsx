40d654b44c822753f98156f8a89a75d9b8c59d37
Sean Morgan <insideseanshead@gmail.com>
2022-01-10 14:11:29 -0800

    added row and col to character stat component and some style

--- src/components/CharacterStats.jsx
+++ src/components/CharacterStats.jsx
@@ -1,55 +1,64 @@
-const CharacterStats = () => {
-    return (
-        <div className="stats">
-            {/* Might */}
-            <div className="might">
-                <div className="mods">
-                    <div className="pool">
-                        12
-                    </div>
-                    <div className="edge">
-                        0
-                    </div>
-                </div>
-                <div className="characterSt">
-                    12
-                </div>
-            </div>
-            {/* speed */}
-            <div className="speed">
-                <div className="mods">
-                    <div className="pool">
-                        16
-                    </div>
-                    <div className="edge">
-                        2
-                    </div>
-                </div>
-                <div className="characterSt">
-                    16
-                </div>
-                <div className="armor">
-                    <div className="cost">1</div>
-                    <div className="armor-value">2</div>
-                </div>
-            </div>
-            {/* Intellect */}
-            <div className="intellect">
-                <div className="mods">
-                    <div className="pool">
-                        13
-                    </div>
-                    <div className="edge">
-                        0
-                    </div>
-                </div>
-                <div className="characterSt">
-                    13
-                </div>
-            </div>
-            
-        </div>
-    )
-}
+import { Row, Col } from "react-bootstrap";
 
-export default CharacterStats
+const CharacterStats = () => {
+  return (
+    <div className="stats">
+      {/* Might */}
+      <div className="might">
+        <Row>
+          <Col xs={3}>
+            <div className="mods">
+              <div className="pool">12</div>
+              <div className="edge">0</div>
+            </div>
+          </Col>
+          <Col xs={4}>
+            <div className="characterStat">12</div>
+          </Col>
+        </Row>
+      </div>
+      {/* speed */}
+      <div className="speed">
+        <Row>
+          <Col xs={3}>
+            <div className="mods">
+              <div className="pool">16</div>
+              <div className="edge">2</div>
+            </div>
+          </Col>
+          <Col xs={4}>
+            <div className="characterStat">16</div>
+          </Col>
+          <Col>
+            <div className="armor">
+              <Row>
+                <Col xs={6}>
+                  <div className="cost">1</div>
+                </Col>
+                <Col xs={6}>
+                  <div className="armor-value">2</div>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </div>
+      {/* Intellect */}
+      <div className="intellect">
+        <Row>
+          <Col xs={3}>
+            <div className="mods">
+              <div className="pool">13</div>
+              <div className="edge">0</div>
+            </div>
+          </Col>
+          <Col xs={4}>
+            <div className="characterStat">13</div>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+};
+
+export default CharacterStats;
